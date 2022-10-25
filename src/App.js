ed388ce2ec25768de9220d40816caff3552b7b2e
Jeremiah Ekanem <jeremyessien@gmail.com>
2022-10-25 12:01:11 +0100

    Reformatted the code

--- src/App.js
+++ src/App.js
@@ -14,8 +14,12 @@ function App() {
       amount: 94.12,
       date: new Date(2020, 7, 14),
     },
-    { //id: e3,
-       title: "Book Purchase", amount: 20, date: new Date(2022, 2, 21) },
+    {
+      //id: e3,
+      title: "Book Purchase",
+      amount: 20,
+      date: new Date(2022, 2, 21),
+    },
   ];
   return (
     <div>
@@ -24,7 +28,7 @@ function App() {
         title={expenses[0].title}
         amount={expenses[0].amount}
         date={expenses[0].date}
-     />
+      />
 
       <ExpenseItem
         title={expenses[1].title}
