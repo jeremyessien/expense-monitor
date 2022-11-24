e4e5fa18226987ef3045356a0f0322999f2ba5f4
Jeremiah Ekanem <jeremyessien@gmail.com>
2022-11-24 14:56:18 +0100

    updated

--- src/components/Expenses/ExpenseFilter.js
+++ src/components/Expenses/ExpenseFilter.js
@@ -1,21 +1,24 @@
 import React from "react";
 
-import './ExpenseFilter.css';
+import "./ExpenseFilter.css";
 
-function ExpenseFilter () {
+function ExpenseFilter(props) {
+  function yearChangeHandler(event) {
+    props.onChangeFilter(event.target.value);
+  }
 
-    return (
-        <div className="expense-filter">
-            <div className="expenses-filter__control ">
-                <label> Filter by year</label>
-                <select>
-                    <option value={'2022 > 2022' }></option>
-                    <option value={'2021 > 2021' }></option>
-                    <option value={'2020 > 2020' }></option>
-                    <option value={'2019 > 2019' }></option>
-                </select>
-            </div>
-        </div>
-    )
-};
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control ">
+        <label> Filter by year</label>
+        <select onChange={yearChangeHandler}>
+          <option value="2022"> 2022</option>
+          <option value="2021"> 2021 </option>
+          <option value="2020"> 2020 </option>
+          <option value="2019"> 2019</option>
+        </select>
+      </div>
+    </div>
+  );
+}
 export default ExpenseFilter;
